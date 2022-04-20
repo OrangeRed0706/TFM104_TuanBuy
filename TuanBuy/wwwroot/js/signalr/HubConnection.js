e26832c20cc3d0944bf9eebb1db87c86cc516a5b
BennyHunag <p1472222@gmail.com>
2022-04-21 00:15:26 +0800

    OK

---
+++ TuanBuy/wwwroot/js/signalr/HubConnection.js
@@ -0,0 +1,19 @@
+﻿////var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+////// 傳送通知事件
+//////connection.on("ReceiveMessage", function (user, message) {
+//////    var msg = message;
+//////    document.getElementById("notifyBell").click();
+//////    layout.message = user+"：" + msg;
+//////    console.log("message:" + msg);
+//////});
+////// 連接事件
+//////
+////connection.start().then((res) => {
+////    console.log("連線成功");
+////    connection.invoke("GetAllNotify").then(() => {
+////        console.log("取得通知成功！");
+
+////    });
+////}).catch(function (err) {
+////    return console.error(err.toString());
+////});
